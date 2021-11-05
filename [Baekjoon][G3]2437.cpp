96f668ee5f6ad71fbfa57c172d2ecbdacf248acb
hhyeong <hjy7833@naver.com>
2021-11-05 17:13:19 +0900

    test

---
+++ [Baekjoon][G3]2437.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#define SIZE 100
+
+int main(void) {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+	int n;
+	int scale[SIZE];
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		std::cin >> scale[i];
+	}
+}
