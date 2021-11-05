5db614b46bba75d8de88e2df1057cda467e6a866
hhyeong <hjy7833@naver.com>
2021-11-05 18:28:56 +0900

    11/5

--- Baekjoon/[Baekjoon][G3]2437.cpp
+++ Baekjoon/[Baekjoon][G3]2437.cpp
@@ -1,14 +1,24 @@
 ﻿#include <iostream>
+#include <algorithm>
 #define SIZE 100
 
 int main(void) {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
-	int n;
+	int n,sum=0;
 	int scale[SIZE];
+	/* input data */
 	std::cin >> n;
 	for (int i = 0; i < n; i++) {
 		std::cin >> scale[i];
 	}
+	std::sort(scale, scale + n);
+	for (int i = 0; i < n; i++) {
+		if (sum + 1 >= scale[i])
+			sum += scale[i];
+		else
+			break;
+	}
+	std::cout << sum;
 }
